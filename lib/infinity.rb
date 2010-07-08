037ac4552459620a0bd092de946bc670bdf413da
Daniel Bretoi <daniel@netwalk.org>
2010-07-07 19:57:10 -0700

    added more arithmatic

--- lib/infinity.rb
+++ lib/infinity.rb
@@ -12,6 +12,8 @@ class Infinity
       self
     end
     def *(obj)
+      return 0 if obj == 0 or obj == 0.0
+      return nil if obj == nil
       self
     end
     def >(obj)
@@ -27,36 +29,4 @@ class Infinity
   end
   include Methods
   extend Methods
-end
-
-if $0 == __FILE__
-  eval DATA.read, nil, $0, __LINE__+4
-end
-
-__END__
-
-require 'test/unit'
-
-class TC_Infinity < Test::Unit::TestCase
-  def test_arithmatic
-    assert_equal(Infinity, (Infinity + 23))
-    assert_equal(Infinity, (Infinity - 23))
-    assert_equal(Infinity, (Infinity / 23))
-    assert_equal(Infinity, (Infinity * 23))
-    assert_equal(1, (Infinity / Infinity))
-    assert_equal(1, (Infinity.new / Infinity))
-    assert_equal(1, (Infinity.new / Infinity.new))
-  end
-
-  def test_comparable
-    assert(Infinity > 23)
-    assert_equal(false, Infinity < 23)
-    assert(Infinity == Infinity)
-  end
-
-  def test_string
-    assert_equal("Infinity", "#{Infinity}")
-    assert_equal("Infinity", Infinity.to_s)
-  end
-
 end
--- test/test_infinity.rb
+++ test/test_infinity.rb
@@ -1,7 +1,32 @@
 require 'helper'
 
 class TestInfinity < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "test arithmatic" do
+    assert_equal(Infinity, (Infinity + 23))
+    assert_equal(Infinity, (Infinity - 23))
+    assert_equal(Infinity, (Infinity / 23))
+    assert_equal(Infinity, (Infinity * 23))
+    assert_equal(1, (Infinity / Infinity))
+    assert_equal(1, (Infinity.new / Infinity))
+    assert_equal(1, (Infinity.new / Infinity.new))
+    assert_equal(0, Infinity * 0)
+    assert_equal(nil, Infinity * nil)
+  end
+
+  should "test comparable" do
+    assert(Infinity > 23)
+    assert_equal(false, Infinity < 23)
+    assert(Infinity == Infinity)
+  end
+
+  should "test string" do
+    assert_equal("Infinity", "#{Infinity}")
+    assert_equal("Infinity", Infinity.to_s)
+  end
+
+  context "zero" do
+    should "divide by zero" do
+      assert_equal(Infinity, 9/0)
+    end
   end
 end
